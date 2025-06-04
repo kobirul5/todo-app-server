774c8402721d52b8982d672cf8ea292fe3224816
Md. Kobirul Islam <kobirul05j@gmail.com>
2025-06-04 15:36:42 +0600

    savbe data in mongodb

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,12 +1,16 @@
 import app from "./app";
+import { client } from "../config/mongodb";
 const port = 5000
 
 let server;
 
-const bootstrap= async()=>{
-    server = app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const bootstrap = async () => {
+  await client.connect();
+  console.log("connected DB")
+
+  server = app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
 }
 
 bootstrap()
--- src/app/todos/todos.router.ts
+++ src/app/todos/todos.router.ts
@@ -1,17 +1,27 @@
-import express, {Request, Response } from "express"
-import fs from "fs";
-import path from "path";
-
-const filePath = path.join(__dirname, "../../../db/todo.json")
+import express, { Request, Response } from "express"
+import { client } from "../../config/mongodb"
 
 export const todosRouter = express.Router()
 
-todosRouter.get('/', (req: Request, res: Response) => {
-  const data = fs.readFileSync(filePath, {encoding:"utf-8"})
-  res.send(data)
+
+
+todosRouter.get('/',async (req: Request, res: Response) => {
+    const db = await client.db("todosDB")
+    const collection = await db.collection("todos")
+
+    await collection.insertOne({
+        title: "mongo DB",
+        description: "Learning Mongo DB",
+        priority: "high",
+        isCompleted: false
+    })
+    const cursor = collection.find({})
+    const todos = await cursor.toArray()
+
+    res.json(todos)
 })
 todosRouter.post('/create-todo', (req: Request, res: Response) => {
-  const {title, body} = req.body
-  console.log(title,",,,,,", body)
-  res.send("done")
+    const { title, body } = req.body
+    console.log(title, ",,,,,", body)
+    res.send("done")
 })
