82d718f58023e369ec79d7ff166e5a0a729e145a
Md. Kobirul Islam <kobirul05j@gmail.com>
2025-06-04 12:54:28 +0600

    added basic  get and post api

--- dist/app/server.js
+++ dist/app/server.js
@@ -13,7 +13,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-const port = 3000;
+const port = 5000;
 let server;
 const bootstrap = () => __awaiter(void 0, void 0, void 0, function* () {
     server = app_1.default.listen(port, () => {
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,23 @@
 import express, { Request, Response } from "express"
+import fs from "fs"
+import path from "path"
 const app = express()
+app.use(express.json())
 
+const filePath = path.join(__dirname, "../../db/todo.json")
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+  res.send('Welcome To ToDos Apps')
+})
+
+app.get('/todos', (req: Request, res: Response) => {
+  const data = fs.readFileSync(filePath, {encoding:"utf-8"})
+  res.send(data)
+})
+app.post('/todos/create-todo', (req: Request, res: Response) => {
+  const {title, body} = req.body
+  console.log(title,",,,,,", body)
+  res.send("done")
 })
 
 export default app;
